8fabb9eae35e6d97531a6b9f38f8c63d4db97f3d
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-06-13 01:49:18 +0500

    pointer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import image from '../assets/hero.png'
+import header from '../assets/header.png'
 import arrow from '../assets/arrow.png'
 
 const Hero = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  console.log(windowWidth);
   return (
     <div className='relative'>
-      <img src={image} alt="" />
+      <img className={`h-[300px] md:h-[550px] w-[700px] md:w-full ${!windowWidth > 700 && 'object-cover'}`} src={windowWidth > 770 ? image : header} alt="" />
       <div className='container'>
-      <div className='absolute top-[100px]'>
-        <p className='text-[28px] font-semibold'>Улучшим любые условия</p>
-        <p className='text-[60px] font-bold'>TANK 500</p>
-        <p className='text-[28px] font-semibold'>Осталось всего 5 автомобилей!</p>
-        <div className='bg-main-yellow w-[246px] py-[18px] rounded-[8px] flex justify-center gap-2 mt-10 cursor-pointer items-center'>
+      <div className='md:absolute top-[100px] mt-5 md:mt-0'>
+        <p className='md:text-[28px] text-[18px] font-semibold'>Улучшим любые условия</p>
+        <p className='md:text-[60px] text-[40px] font-bold'>TANK 500</p>
+        <p className='md:text-[28px] text-[18px] font-semibold'>Осталось всего 5 автомобилей!</p>
+        <div className='bg-main-yellow md:w-[246px] w-full py-[18px] rounded-[8px] flex justify-center gap-2 mt-10 cursor-pointer items-center'>
             <p className='text-[14px] font-semibold text-black'>Получить предложение</p>
             <img src={arrow} alt="" />
         </div>
---
+++ src/components/Pointer.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import day from '../assets/day.svg'
+import arrow from '../assets/arrow.png'
+import medal from '../assets/medal.png'
+import note from '../assets/note.png'
+import timer from '../assets/timer.png'
+import red from '../assets/red.png'
+import white from '../assets/white.png'
+
+const Pointer = () => {
+    const data = [
+        {
+            img: medal,
+            title: 'Официальный дилер',
+            info: "Гарантируем высокое качество обслуживания."
+        },
+        {
+            img: timer,
+            title: 'ПОКУПКА АВТО ЗА 1 ДЕНЬ',
+            info: "Удобный процесс покупки, включая оформление всех документов."
+        },
+        {
+            img: note,
+            title: 'ВСЕ КОМПЛЕКТАЦИИ В НАЛИЧИИ',
+            info: "Широкий выбор комплектаций, с полным пакетом документов"
+        },
+    ]
+    return (
+        <div className='container'>
+            <div className='bg-secondary-dark md:flex items-center justify-between rounded-[10px] md:px-10 px-4 py-5 mt-5 mb-[60px]'>
+                <p className='w-[318px] md:text-[30px] text-[28px] font-bold leading-[30px]'>Срок действия спецпредложения:</p>
+                <img className='my-6 md:my-0' src={day} alt="rasm" />
+                <div className='bg-main-yellow pb-[18px] pt-[14px] rounded-[8px] md:w-[248px] flex gap-2 items-center justify-center'>
+                    <p className='text-black text-[14px] font-semibold'>Узнать цену с выгодами</p>
+                    <img className='mt-1' src={arrow} alt="rasm" />
+                </div>
+            </div>
+            <div className='md:flex gap-[10px] justify-between'>
+                {data.map(item => (
+                    <div className='md:w-[416px] w-full border rounded-[10px] p-5 flex md:items-start items-center gap-7 mb-[10px] md:mb-0' key={item.title}>
+                        <img src={item.img} alt="rasm" />
+                        <div>
+                            <p className='text-[20px] font-bold md:mb-[14px] leading-[22px]'>{item.title}</p>
+                            <p className='hidden md:block'>{item.info}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <p className='md:w-[1158px] w-full m-auto md:text-[40px] text-[28px] font-bold text-center uppercase mt-[100px] mb-10'>ЗАБРОНИРУЙТЕ Автомобиль СЕГОДНЯ И ПОЛУЧИТЕ ДОПОЛНИТЕЛЬНУЮ ВЫГОДУ 100 000 ₽</p>
+            <div className='flex justify-center gap-6 mb-[60px]'>
+                <div className='flex flex-col items-center'>
+                    <img className='w-[140px] md:w-[196px]' src={red} alt="rasm" />
+                    <p>TANK 300</p>
+                </div>
+                <div className='flex flex-col items-center'>
+                    <img className='w-[140px] md:w-[196px]' src={white} alt="rasm" />
+                    <p>TANK 500</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pointer
