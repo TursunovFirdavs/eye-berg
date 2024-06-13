37f20ee0edbdbe1189e97e58fc4283e6fef9ab8e
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-06-13 20:56:11 +0500

    buttons

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import arrow from '../assets/arrow.png'
+import { IoClose } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
+
 
 const Offer = () => {
+    const navigate =useNavigate()
   return (
     <div className='flex items-center justify-center md:h-[100vh]'>
-      <div className='bg-secondary-dark md:w-[555px] w-[345px] md:px-[80px] px-4 my-[100px] md:my-0 rounded-[10px] py-10'>
-        <p className='text-[30px] font-bold text-center'>Получить предложение</p>
-        <p className='text-center mt-4 mb-10'>Пожалуйста, укажите свои данные. Наш менеджер свяжется с вами в течение 15 минут.</p>
+      <div className='bg-secondary-dark md:w-[555px] w-[345px] relative md:px-[80px] px-4 my-[100px] md:my-0 rounded-[10px] py-10'>
+        <p className='text-[30px] font-bold text-center leading-[30px]'>Получить предложение</p>
+        <p className='text-center mt-4 mb-10 w-[266px] md:w-[380px] m-auto'>Пожалуйста, укажите свои данные. Наш менеджер свяжется с вами в течение 15 минут.</p>
         <p className='text-[14px] text-text-gray'>Модель</p>
         <select className='w-full h-[52px] rounded-[8px] bg-secondary-dark border border-gray-500 mt-2 mb-6 ' name="" id="Модель"></select>
         <p className='text-[14px] text-text-gray'>Телефон</p>
@@ -19,6 +23,7 @@ const Offer = () => {
             <input className='w-[14px] h-[14px] bg-secondary-dark border mt-2 md:mt-0 border-gray-500 rounded-[4px]' type="checkbox" />
             <p>Согласен на обработку персональных данных.</p>
         </div>
+        <IoClose onClick={() => navigate(-1)} className='absolute top-5 cursor-pointer right-5 text-2xl' />
       </div>
     </div>
   )
